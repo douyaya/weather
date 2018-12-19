0aedb7e7497bab2a69fe44e44e44a8ee91db0fd9
douyay <a780525690@163.com>
2018-12-19 15:50:44 +0800

    first time

---
+++ src/WeatherHeader/WeatherHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './WeatherHeader.css'
+class WeatherHeader extends Component {
+  render() {
+    return (
+      <div className="weather-header">
+        toubu 
+      </div>
+    );
+  }
+}
+
+export default WeatherHeader;
